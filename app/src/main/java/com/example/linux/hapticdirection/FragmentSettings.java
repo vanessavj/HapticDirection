bed227eda98fe2e36b79a942addfe2f7d74b771b
vanessavj <vanessa.vj@web.de>
2016-08-22 09:06:13 +0200

    First commit of HapticDirection. Two Fragments, namely FragmentSettings and MapFragment.

---
+++ app/src/main/java/com/example/linux/hapticdirection/FragmentSettings.java
@@ -0,0 +1,14 @@
+package com.example.linux.hapticdirection;
+
+import android.app.Fragment;
+import android.os.Bundle;;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentSettings extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/linux/hapticdirection/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.linux.hapticdirection;
+
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.MapFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void  selectFrag(View view){
+        Fragment fragment;
+        if(view==findViewById(R.id.einstellungen)){
+            fragment=new FragmentSettings();
+        } else{
+           fragment=new MapFragment();
+        }
+
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.place_holder, fragment);
+        fragmentTransaction.commit();
+    }
+}
